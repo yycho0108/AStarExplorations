e195634ad98fa32e1569388e3795840a9a9f42e1
yycho0108 <jchocholate@gmail.com>
2020-03-15 22:21:39 -0400

    add test scripts

---
+++ astarcpp/graph/test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import bglpy as g
+import numpy as np
+import sys
+import time
+
+from line_profiler import LineProfiler
+
+from phonebot.core.kinematics.workspace import get_workspace
+from phonebot.core.kinematics.workspace import rasterize
+from matplotlib import pyplot as plt
+
+
+state = {'mdown' : False }
+def main():
+    grid = ~rasterize(get_workspace(return_poly=True, lower_half=False), 512)
+    v0 = np.int32([39,43]).reshape(2,1)
+    v1 = np.int32([147,101]).reshape(2,1)
+
+    start = time.time()
+    for _ in range(100):
+        path = g.FindPath(grid, v0, v1)
+    # print(path)
+    end = time.time()
+    print( (end - start) / 100 )
+    # print(path)
+
+
+def gui():
+    grid = ~rasterize(get_workspace(return_poly=True, lower_half=False), 2048)
+    print(grid.dtype)
+
+    fig = plt.gcf()
+    h, = plt.plot([],[],'rx-')
+    def on_click(event):
+        global state
+        j, i = int(event.xdata), int(event.ydata)
+        if state['mdown']:
+            v0 = np.int32([state['i'], state['j']]).reshape(2,1)
+            v1 = np.int32([i, j]).reshape(2,1)
+            print(v0,v1)
+            
+            t0=time.time()
+            path = g.FindPath(grid, v0, v1)
+            t1=time.time()
+            print(t1-t0)
+
+            if len(path) > 0:
+                print(path)
+                path = np.concatenate(path, axis=1)
+                h.set_data(path[::-1])
+                plt.pause(0.001)
+            state['mdown'] = False
+        else:
+            state['mdown'] = True
+            state['i'] = i
+            state['j'] = j
+
+    fig.canvas.mpl_connect('button_press_event', on_click)
+    plt.imshow(grid)
+    plt.show()
+
+if __name__ == '__main__':
+    # main()
+    gui()
+
+    #prof = LineProfiler()
+    #prof.add_function(g.FindPath)
+    #prof.enable_by_count()
+    # prof.add_function(g.FindPath)
+    # prof.print_stats()
---
+++ astarcpp/graph/test_rrt.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import bglpy as g
+from matplotlib import pyplot as plt
+import sys
+import time
+
+if False:
+    xs, ys = np.mgrid[-1:1:0.01, -1:1:0.01]
+    sdf = g.SdfCircle(g.Node2D(0, 0), 0.1)
+    print(sdf(g.Node2D(0, 2)))  # 3.9?
+    sys.exit(0)
+    dmap = np.zeros_like(xs)
+    for i, j in np.ndindex(dmap.shape):
+        x, y = xs[i, j], ys[i, j]
+        d = sdf(g.Node2D(x, y))
+        dmap[i, j] = d
+        print(x, y)
+        print(d, np.sqrt(x*x+y*y) - 0.1)
+    print(dmap.max())
+    print(dmap.min())
+    plt.imshow(dmap)
+    plt.show()
+    sys.exit(0)
+
+
+def get_workspace_sdf():
+    kHipJointOffset = 0.0110
+    kKneeLinkLength = 0.0285
+    kHipLinkLength = 0.0175
+    kSmallRadius = kKneeLinkLength - kHipLinkLength
+    kLargeRadius = kKneeLinkLength + kHipLinkLength
+
+    ca = g.Node2D(kHipJointOffset, 0)  # (0.011, 0.0)
+    cb = g.Node2D(-kHipJointOffset, 0)  # (-0.011, 0.0)
+
+    f1 = g.SdfCircle(ca, kSmallRadius)
+    f2 = g.SdfCircle(cb, kSmallRadius)
+    f3 = g.SdfCircle(ca, kLargeRadius)
+    f4 = g.SdfCircle(cb, kLargeRadius)
+
+    # if interior is negative ...
+    # return g.SdfIntersect(
+    #        g.SdfVector([g.SdfNegate(f1),g.SdfNegate(f2),f3,f4]))
+
+    # if interior is positive ...
+    return g.SdfUnion(
+        g.SdfVector([f1, f2, g.SdfNegate(f3), g.SdfNegate(f4)]))
+
+
+def get_distance_map(sdf):
+    mnx, mny = -0.035, -0.044
+    mxx, mxy = 0.035, 0.044
+    xs, ys = np.mgrid[mnx:mxx:0.001, mny:mxy:0.001]
+    dmap = np.zeros_like(xs)
+    for i, j in np.ndindex(dmap.shape):
+        x, y = xs[i, j], ys[i, j]
+        d = sdf(g.Node2D(x, y))
+        dmap[i, j] = d
+    return dmap
+
+
+sdf = get_workspace_sdf()
+s = g.RrtSettings()
+s.workspace = g.Workspace(g.Node2D(-0.035, -0.044), g.Node2D(0.035, 0.044))
+s.max_iter = 16384
+
+extent = [-0.035, 0.035, -0.044, 0.044]
+dmap = get_distance_map(sdf)
+rrt = g.Rrt(s, sdf)
+rrt.Seed(512)
+source = g.Node2D(0.0032096, 0.03201624)
+target = g.Node2D(0.01301248, -0.01502493)
+start = time.time()
+path = rrt.GetTrajectory(source, target)
+end = time.time()
+print('Took {} sec'.format(end-start))
+path = np.asarray([(p.x, p.y) for p in path])
+
+plt.imshow(dmap.T, extent=extent)
+plt.imshow(dmap.T > 0, extent=extent, alpha=0.1)
+plt.plot(source.x, source.y, 'rx')
+plt.plot(target.x, target.y, 'b+')
+plt.plot(path[..., 0], path[..., 1])
+plt.axis('equal')
+plt.gca().set_aspect('equal')
+plt.show()
